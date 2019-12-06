53b308a485c4700642e73944c683b8356367ce90
gbengaPS <ioyetade@gmail.com>
2019-12-06 15:45:25 +0100

    fix styling isssues - modify book card - create header

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ class Home extends Component {
     const { booksCategory } = this.props;
     if (booksCategory) {
       const categories = booksCategory.map((category) => {
-        const bestSellers = category.results.books.map(book => (
+        const bestSellers = category.results.books.map((book) => (
           <BookCard
             imageUrl={book.book_image}
             amazonUrl={book.amazon_product_url}
@@ -30,12 +30,22 @@ class Home extends Component {
         return (
           <div key={category.results.display_name}>
             <h2>{category.results.list_name}</h2>
-            <div className="books-scroll">{bestSellers}</div>
+            <div className="books-scroll">
+              {bestSellers}
+              <p>&nbsp;</p>
+            </div>
           </div>
         );
       });
 
-      return <div>{categories}</div>;
+      return (
+        <div>
+          <header>
+            <h1>New York Times Best Sellers </h1>
+          </header>
+          <main>{categories}</main>
+        </div>
+      );
     }
     return <Layout>Hello world</Layout>;
   }
