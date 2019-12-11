955973c8abf31e5c1a23324229f25df583d6d241
gbengaPS <ioyetade@gmail.com>
2019-12-11 10:29:43 +0100

    fix deployment configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const withSass = require('@zeit/next-sass');
 require('dotenv').load();
 
 module.exports = {
+  exportPathMap: () => ({
+    '/': { page: '/' },
+  }),
+
   publicRuntimeConfig: {
     apiKey: process.env.API_KEY,
   },
