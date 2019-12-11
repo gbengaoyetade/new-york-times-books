727c671d28cd118484343ac7f471008ceab59482
gbengaPS <ioyetade@gmail.com>
2019-12-11 15:59:00 +0100

    add mobile phone responsiveness

--- components/bestSellers.js
+++ components/bestSellers.js
@@ -58,7 +58,7 @@ class BestSellers extends Component {
       ));
       return (
         <div key={category.results.display_name}>
-          <h2 className="center">{category.results.display_name}</h2>
+          <h2>{category.results.display_name}</h2>
           <div className="books-scroll-wrapper">
             <button
               type="button"
