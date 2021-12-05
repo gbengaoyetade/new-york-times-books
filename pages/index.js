1673e51d9d3ef79e7a9be347c87c0673c48a5806
Gbenga Oyetade <ioyetade@gmail.com>
2021-12-05 23:28:27 +0100

    Update app to nextjs 12  (#15)

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,30 @@
 import React, { Component } from 'react';
-import { arrayOf, object } from 'prop-types';
-import Layout from '../components/layout';
 import BestSellers from '../components/bestSellers';
 import makeInitialFetch from '../utils';
 
 class Home extends Component {
   render() {
     const { booksCategory } = this.props;
-    if (booksCategory) {
-      return (
-        <div>
-          <header>
-            <h1>New York Times Best Sellers </h1>
-          </header>
-          <main>
-            <BestSellers booksCategory={booksCategory} />
-          </main>
-        </div>
-      );
+    if (!booksCategory) {
+      return null;
     }
-    return <Layout>...</Layout>;
+
+    return (
+      <div>
+        <header>
+          <h1>New York Times Best Sellers </h1>
+        </header>
+        <main>
+          <BestSellers booksCategory={booksCategory} />
+        </main>
+      </div>
+    );
   }
 }
 
-export async function getServerSideProps() {
-  try {
-    const response = await makeInitialFetch();
-    return { props: { booksCategory: response } };
-  } catch (error) {
-    return error;
-  }
+export async function getStaticProps() {
+  const response = await makeInitialFetch();
+  return { props: { booksCategory: response }, revalidate: 1440 };
 }
 
-Home.propTypes = {
-  booksCategory: arrayOf(object).isRequired,
-};
 export default Home;
